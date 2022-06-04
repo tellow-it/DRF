4245cd884b6a55b53ddf2831bc31ceb50d58b639
Tellow <itikhonov.work@gmail.com>
2022-06-04 18:55:58 +0300

    lesson 14 pagination

--- DRF_study/settings.py
+++ DRF_study/settings.py
@@ -135,7 +135,9 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    ]
+    ],
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    # 'PAGE_SIZE': 3,
 }
 
 SIMPLE_JWT = {
--- main/views.py
+++ main/views.py
@@ -4,6 +4,7 @@ from rest_framework import generics, viewsets
 # Create your views here.
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -11,11 +12,18 @@ from .permissions import *
 from main.models import Student, Group
 from main.serializers import StudentSerializer
 
+class StudentAPIListPagination(PageNumberPagination):
+    page_size = 3
+    page_size_query_param = 'page_size'
+    max_page_size = 5
+
 
 class StudentAPIList(generics.ListCreateAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)  # для авторизованных ползователей
+    pagination_class = StudentAPIListPagination
+
 
 
 class StudentAPIUpdate(generics.RetrieveUpdateAPIView):
